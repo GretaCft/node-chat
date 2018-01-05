e1d1c38ba72d1cbd45243f74251ec907269c97e9
GretaCft <gretasrv@yahoo.es>
2018-01-05 20:28:02 +0100

    enviada a heroku

--- server/server.js
+++ server/server.js
@@ -4,12 +4,12 @@ const express = require('express');
 const socketIO =require('socket.io'); 
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000; 
 
 let app = express();
 let server = http.createServer(app);
 let io = socketIO(server);
 
+app.set('port', (process.env.PORT || 3000));
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
@@ -30,7 +30,7 @@ io.on('connection', (socket) => {
 
 });
 
-server.listen(port, () => {
-    console.log(`Servidor en puerto ${port}`);
+server.listen(app.get('port'), () => {
+    console.log(`Servidor en puerto`, app.get('port'));
 });
 
